42c21b76cdc66f289850de9daee2f93472096976
Beltrox <88000858+Beltrox@users.noreply.github.com>
2024-08-13 16:43:47 +0200

    feat: added navbar
    - Added navbar
    - Navbar changes positions on Browser Language
    - Navbar jumps to pre-defined divs with id

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,28 @@
+const userLanguage = navigator.language || navigator.languages[0];
+
+const rtlLanguages = ['ar', 'he', 'fa', 'ur']; // Liste der RTL-Sprachen
+
+const isRTL = rtlLanguages.includes(userLanguage.split('-')[0]);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('menu');
+
+    if (isRTL) {
+        menu.classList.add('justify-end');
+        menu.classList.remove('justify-start');
+    } else {
+        menu.classList.add('justify-start');
+        menu.classList.remove('justify-end');
+    }
+});
+
 function wechsel_zu_datenschutz (){
     window.location.href="datenschutz.html";
 }
 function wechsel_zu_impressum (){
     window.location.href="impressum.html";
+}
+function wechsel_zu_homepage (){
+    window.location.href="index.html";
 }
