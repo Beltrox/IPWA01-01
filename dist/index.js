6c0c4a59211443ce0d046a6a70a4ad6be5b27917
Beltrox <88000858+Beltrox@users.noreply.github.com>
2024-08-23 18:46:53 +0200

    feat: implemented Table filtering
    -added filter input
    -implemented Table filtering

--- dist/index.js
+++ dist/index.js
@@ -11,13 +11,11 @@ function wechsel_zu_impressum(){
     window.location.href="impressum.html";
 }
 
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('menu');
     const datenschutzButton = document.getElementById("datenschutzButton");
     const impressumButton = document.getElementById("impressumButton");
+
 
     datenschutzButton.addEventListener("click", () => wechsel_zu_datenschutz());
     impressumButton.addEventListener("click", () => wechsel_zu_impressum());
@@ -29,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.remove('justify-end');
     }
 });
+
 loadJsonAndGenerateTable('../resources/statistic1.json', 'table-container1');
 loadJsonAndGenerateTable('../resources/statistic2.json', 'table-container2');
 
@@ -40,13 +39,35 @@ async function loadJsonAndGenerateTable(data, elementId) {
         }
         const dataJson = await response.json();
         const container = document.getElementById(elementId);
-        container.appendChild(generateTable(dataJson));
+        const filterInputField = document.createElement('input')
+        filterInputField.classList.add('w-1/5', 'placeholder-black', 'p-1');
+        filterInputField.setAttribute('placeholder', 'Filter');
+
+        filterInputField.addEventListener("input", () => {regenerateTable(dataJson, elementId, filterInputField.value)});
+        container.appendChild(filterInputField);
+        container.appendChild(generateTable(dataJson, elementId, filterInputField.value));
     } catch (error) {
         console.error('Fehler beim Laden der JSON-Datei:', error);
     }
 }
 
-function generateTable(data) {
+function regenerateTable(dataJson, elementId, filterInput) {
+    const table = document.getElementById(elementId).getElementsByTagName('table');
+    const noResultsMessage = document.getElementById("noResultsP" + elementId);
+
+    if (table[0]) {
+        table[0].remove();
+    }
+
+    if (noResultsMessage) {
+        noResultsMessage.remove();
+    }
+
+    const container = document.getElementById(elementId);
+    container.appendChild(generateTable(dataJson, elementId, filterInput));
+}
+
+function generateTable(data, elementId, filterInput = '') {
     console.log(data);
     const table = document.createElement('table');
     table.classList.add('border', 'border-colapse', 'w-3/4', 'table-fixed');
@@ -66,7 +87,7 @@ function generateTable(data) {
     // Tabelleninhalt erstellen
     const tbody = document.createElement('tbody');
     data.forEach(item => {
-        if(item != data[0]){
+        if(item != data[0] && item[0].toLocaleLowerCase().includes(filterInput.toLocaleLowerCase())){
             const row = document.createElement('tr');
             Object.values(item).forEach(value => {
                 const cell = document.createElement('td');
@@ -77,8 +98,16 @@ function generateTable(data) {
             tbody.appendChild(row);
         }
     });
-    table.appendChild(tbody);
 
+    if(tbody.children.length == 0){
+        let noResultsMessage = document.createElement('p');
+        noResultsMessage.textContent = "Es wurden keine Ergebnisse gefunden";
+        noResultsMessage.id = "noResultsP" + elementId;
+        return noResultsMessage;
+    }
+
+    table.appendChild(tbody);
     return table;
 }
+
 
