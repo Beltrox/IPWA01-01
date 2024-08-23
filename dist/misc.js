dfc378cd7f55b04ba09bf016a8c055006ed20591
Beltrox <88000858+Beltrox@users.noreply.github.com>
2024-08-23 16:23:58 +0200

    feat: Content
    -new Icon size
    -Datenschutz content
    -Impressum content

--- dist/misc.js
+++ dist/misc.js
@@ -12,8 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const datenschutzButton = document.getElementById("datenschutzButton");
     const impressumButton = document.getElementById("impressumButton");
     const zurueckButton = document.getElementById("zurueckButton");
+    const logoButton = document.getElementById("logo");
 
     datenschutzButton.addEventListener("click", () => wechsel_zu_datenschutz());
     impressumButton.addEventListener("click", () => wechsel_zu_impressum());
     zurueckButton.addEventListener("click", () => wechsel_zu_homepage());
+    logoButton.addEventListener("click", () => wechsel_zu_homepage());
 });
